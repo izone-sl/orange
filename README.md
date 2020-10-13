1f1eccf377b2cb56b95b2c6a0c5b0e6b7f6c8b64
Mohamed Raza <59387235+Raza9798@users.noreply.github.com>
2020-10-13 20:20:40 +0530

    init project with vuetify and heroku server configured

---
+++ README.md
@@ -0,0 +1,19 @@
+# ikmanclone
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/DefaultView.vue
@@ -0,0 +1,82 @@
+<template>
+  <v-row class="mt-5">
+    <v-col>
+      <v-container class="mt-5 pt-5 card indigo lighten-4 p-5">
+        <v-row>
+            <v-col class=" text-center">
+                <h2 class="text-dark">UNDER DEVELOPING ...</h2>
+            </v-col>
+        </v-row>
+        <div class="text-center">
+          <v-progress-circular
+            :rotate="360"
+            :size="100"
+            :width="15"
+            :value="value"
+            color="teal"
+          >
+            {{ value }}
+          </v-progress-circular>
+
+          <v-progress-circular
+            :rotate="-90"
+            :size="100"
+            :width="15"
+            :value="value"
+            color="primary"
+          >
+            {{ value }}
+          </v-progress-circular>
+
+          <v-progress-circular
+            :rotate="90"
+            :size="100"
+            :width="15"
+            :value="value"
+            color="red"
+          >
+            {{ value }}
+          </v-progress-circular>
+
+          <v-progress-circular
+            :rotate="180"
+            :size="100"
+            :width="15"
+            :value="value"
+            color="pink"
+          >
+            {{ value }}
+          </v-progress-circular>
+        </div>
+      </v-container>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      interval: {},
+      value: 0,
+    };
+  },
+  beforeDestroy() {
+    clearInterval(this.interval);
+  },
+  mounted() {
+    this.interval = setInterval(() => {
+      if (this.value === 100) {
+        return (this.value = 0);
+      }
+      this.value += 10;
+    }, 1000);
+  },
+};
+</script>
+
+<style scoped>
+.v-progress-circular {
+  margin: 1rem;
+}
+</style>
